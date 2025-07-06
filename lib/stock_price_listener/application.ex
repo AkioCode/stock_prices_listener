b6ce496be58ef036ae422be9fda249c1fdd56930
Rodrigo Otsuka <akio2631@gmail.com>
2025-07-06 17:34:23 -0300

    Adds LibCluster to auto connect nodes

--- lib/stock_price_listener/application.ex
+++ lib/stock_price_listener/application.ex
@@ -7,10 +7,18 @@ defmodule StockPriceListener.Application do
 
   @impl true
   def start(_type, _args) do
+    topologies = [
+      example: [
+        strategy: Cluster.Strategy.Epmd,
+        config: [hosts: [:"node1@127.0.0.1", :"node2@127.0.0.1", :"node3@127.0.0.1", :"node4@127.0.0.1"]]
+      ]
+    ]
+
     children = [
       # Starts a worker by calling: StockPriceListener.Worker.start_link(arg)
       # {StockPriceListener.Worker, arg}
       {Phoenix.PubSub, name: StockPriceListener.PubSub},
+      {Cluster.Supervisor, [topologies, [name: StockPriceListener.ClusterSupervisor]]},
       {StockPriceListener, []}
     ]
 
