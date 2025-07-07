9f4a1f3a7ccaaba7e0847f2d4135e32fd2c29eda
Rodrigo Otsuka <akio2631@gmail.com>
2025-07-06 21:40:57 -0300

    Implement a stock prices generator

---
+++ lib/stock_price_generator.ex
@@ -0,0 +1,23 @@
+defmodule StockPriceGenerator do
+  use GenServer
+
+  @impl true
+  def init(period) do
+    Process.send_after(self(), {:loop, period}, period)
+    {:ok, []}
+  end
+
+  @impl true
+  def handle_info({:loop, period}, state) do
+    loop(period)
+    {:noreply, state}
+  end
+
+  defp loop(period) do
+    Enum.each(~w(fb amz aapl nvda goog), fn ticker ->
+      price = Enum.random(1000..20000)
+      Phoenix.PubSub.broadcast(StockPriceListener.PubSub, ticker, {ticker, price})
+    end)
+    Process.send_after(self(), {:loop, period}, period)
+  end
+end
